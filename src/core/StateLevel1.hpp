d8e4d15d0e9f04aeb0a1eb0da4bb587bd77ee206
Seth Berrier <berriers@uwstout.edu>
2023-11-21 02:14:06 +0000

    Updated notes, pointer adrs in place

--- src/core/StateLevel1.hpp
+++ src/core/StateLevel1.hpp
@@ -4,22 +4,21 @@
 
 namespace totksavs::core {
     class StateLevel1 : public StateStorage {
-    public:
-        void ReadFromGame() override;
-        void WriteToGame() override;
-        void ReadFromFile(fs::ConfigFile& file, u32 version) override;
-        void WriteToFile(fs::ConfigFile& file) const override;
+        public:
+            void ReadFromGame() override;
+            void WriteToGame() override;
+            void ReadFromFile(fs::ConfigFile& file, u32 version) override;
+            void WriteToFile(fs::ConfigFile& file) const override;
 
-    private:
-        u32 mHealth;
-        f32 mStamina;
-        f32 mHavokPosition[3];
-        f32 mMainPositionMatrix[12];
-        f32 mCameraPanMatrix[12];
-        f32 mCameraZoom;
-        f32 mCameraTilt;
-        // Extras:
-        // reset rune cooldown
-        // TODO reset fall damage
+        private:
+            u32 mHealth;
+            f32 mStamina;
+            f32 mMainPositionMatrix[12];
+
+            // TODO: Haven't found these pointers yet
+            // f32 mHavokPosition[3];
+            // f32 mCameraPanMatrix[12];
+            // f32 mCameraZoom;
+            // f32 mCameraTilt;
     };
 }  // namespace totksavs::core
